3969e22bee261fc091a657a33aae876e475b060e
Matheus Gonçalves <souzagonc@gmail.com>
2018-04-03 16:13:20 -0300

    adding components

---
+++ addon/components/dashboards/vis-wrapper.js
@@ -0,0 +1,6 @@
+import Component from '@ember/component';
+import layout from '../../templates/components/dashboards/vis-wrapper';
+
+export default Component.extend({
+  layout
+});
---
+++ app/components/dashboards/load-visualizations.js
@@ -0,0 +1,1 @@
+export { default } from 'lemonade-datavis/components/dashboards/load-visualizations';
