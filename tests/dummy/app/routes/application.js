9e3973ae3b6e44db1c6b7c6c9a3886d8ddf5404d
Pedro Bernardina <l.pedro.nascimento@gmail.com>
2018-04-23 15:11:04 -0300

    Creates a router for visualizations in development

--- tests/dummy/app/routes/application.js
+++ tests/dummy/app/routes/application.js
@@ -2,20 +2,5 @@ import Route from '@ember/routing/route';
 import $ from 'jquery';
 
 export default Route.extend({
-  beforeModel(){
-    this._super(...arguments);
-    $.ajaxSetup({
-      headers: {
-        'X-Auth-Token': '123456',
-      }
-    });
-  },
-
-  model(){
-    return $.ajax({
-      url: 'https://teste.ctweb.inweb.org.br/caipirinha/dashboards/47',
-      type: 'GET'
-    });
-  },
 })
 
---
+++ tests/dummy/app/routes/map.js
@@ -0,0 +1,23 @@
+import Route from '@ember/routing/route';
+import $ from 'jquery';
+
+export default Route.extend({
+  beforeModel(){
+    this._super(...arguments);
+    $.ajaxSetup({
+      headers: {
+        'X-Auth-Token': '123456',
+      }
+    });
+  },
+
+  model(params){
+    const path = '/data/visualizations';
+    const dataUrl = params['data'] ? `${path}/${params['data']}` : `${path}/pie-chart.json`;
+    return $.ajax({
+      url: dataUrl,
+      type: 'GET'
+    });
+  },
+})
+
